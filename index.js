9431501aa9b709141d8933f5d2df487d1d3b07a8
andrey <tritg@yandex.ru>
2025-03-07 15:32:58 +0300

    change index.html

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-let buttons-tables = document.getElementsByTagName("button");
-for (const element of buttons) {
-    element.addEventListener("click", function (element) {
-        console.log(element);
-        // console.log(element.target.parentElement.previousElementSibling.className);
-        // let targetButton = element.target;
-        // let targetParenClass = element.target.targetButton.parentElement.previousElementSibling.className;
+document.addEventListener('DOMContentLoaded', function () {
 
+    const tables = document.querySelectorAll('table');
+    console.log(tables);
 
-        switch (element) {
-            case 'main-table': console.log(targetParenClass + "  fdesfd");
-            default: console.log(element.prototype + "  ваапв");
-        }
-    });
-}
-
+});
